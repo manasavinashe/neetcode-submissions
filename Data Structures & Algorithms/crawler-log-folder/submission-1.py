0b88b4902195596ea5c0356e2cd4870af5c67f0a
Manas Avinashe <46149191+manasavinashe@users.noreply.github.com>
2026-09-20 14:46:38 +0530

    Add: crawler-log-folder - submission-1

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        depth = 0 
+
+        for i in logs: 
+            if i =="../":
+                if depth > 0 :
+                    depth -=1 
+            elif i =="./":
+                pass
+            else : 
+                depth +=1 
+            
+        
+        return depth 
+
+        
+        
